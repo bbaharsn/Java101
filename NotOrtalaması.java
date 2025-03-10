1fa85ed56b7231cedca9a5ed343563c72d3c38ed
Bahar Şen <bahar_sen09@icloud.com>
2025-03-10 19:40:39 +0300

    patika.com

---
+++ NotOrtalaması.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class NotOrtalamasi{
+    public static void main(String[] args) {
+        double vize, sonsinav;
+        double sonuç;
+
+    Scanner inp=new Scanner(System.in);
+
+    System.out.print(" vize notunuz:");
+    vize=inp.nextDouble();
+    
+    System.out.print(" final notunuz:");
+    sonsinav=inp.nextDouble();
+
+    sonuç=(vize*0.4)+(sonsinav*0.6);
+    System.out.println("ortalamaniz:"+sonuç);
+    if(sonuç>=70){
+        System.out.println("başarili");
+
+    }else{
+        System.out.println("başarisiz");
+    }
+}
+}
